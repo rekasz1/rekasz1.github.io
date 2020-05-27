5088f0e0cfa33722d685cf9b1ce4600e492bac73
Reka Sz <rekabrigitta.sz@gmail.com>
2020-05-27 12:09:46 +0200

    new project

---
+++ js/myfirstgallery.js
@@ -0,0 +1,7 @@
+/*let data = {
+  photo: 'img/redstones.jpg',
+  title: 'Red Stone Gate',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+};
+
+$('#photo').attr('src', data.photo);
