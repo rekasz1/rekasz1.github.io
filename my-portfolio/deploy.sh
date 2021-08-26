a3070a21679e3f77fab3b93e83ab503a69a56123
Reka Szenes <rekabrigitta.sz@gmail.com>
2021-08-26 12:30:05 +0200

    add

--- my-portfolio/deploy.sh
+++ my-portfolio/deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:rekasz1.github.io/rekasz1.github.io.github.io.git master
+git push -f git@github.com:rekasz1/rekasz1.github.io.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
