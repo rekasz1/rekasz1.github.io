d6b54f6822ac3fd52423a1c0da374759bcecaf44
rekasz1 <59920044+rekasz1@users.noreply.github.com>
2021-08-28 10:43:25 +0200

    restyling about me and home pages (#13)

--- js/sitedescription.js
+++ js/sitedescription.js
@@ -7,7 +7,7 @@ export function createsiteDescription() {
     portrait.setAttribute('id', 'portrait')
 
     let title = document.createElement('h1');
-    title.style.color = "rgb(106, 117, 126)";
+    title.style.color = "rgb(252, 252, 255)";
     title.innerText = '<Hi, I’m Reka, Front-end Developer/>';
     title.setAttribute('id', 'glitch')
 
@@ -15,7 +15,7 @@ export function createsiteDescription() {
     let description = document.createElement('p');
     description.innerText = "You’ve found my personal corner of the internet - everything you want to know about me is here."
     // description.style.fontSize = "30px"
-    description.style.color = "#515E69"
+    description.style.color = "#fcfcff"
 
 
     let mainDiv = document.getElementsByTagName('div')[0];
@@ -26,29 +26,5 @@ export function createsiteDescription() {
     contentDiv.appendChild(portrait);
     contentDiv.appendChild(title);
     contentDiv.appendChild(description);
-
-
-    //span for glitch effect
-    // let firstSpan = document.createElement('span')
-    // firstSpan.innerText = '<Hi, I’m  Reka,  Front-end Developer/>'
-    // firstSpan.style.color = "#6a757e"
-
-    // let secondSpan = document.createElement('span')
-    // secondSpan.innerText = '<Hi, I’m  Reka,  Front-end Developer/>'
-    // secondSpan.style.color = "#6a757e"
-    // span.setAttribute('aria-hidden' "true")
-    // span.ariaHidden = true;
-    // console.log(span.ariaHidden)
-
-    // let text = document.createElement('a')
-    // text.innerText = '<Hi, I’m  Reka,  Front-end Developer/>';
-
-
-    // let spanOfPElement = document.getElementById('glitch')
-    //  spanOfPElement.appendChild(firstSpan)
-    // spanOfPElement.appendChild(text)
-    // // spanOfPElement.innerText = '<Hi, I’m  Reka, \n Front-end Developer/>';
-    // spanOfPElement.appendChild(secondSpan)
-
 
 }
